478c2088e4e282de14afc263517f37f845c2b3c9
MochHelmiHasan6 <kichi.hideto@gmail.com>
2022-06-09 15:48:37 +0700

    update akun

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,15 +5,15 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                    <a href="{{ route('pengguna.index') }}">
+                        <img src="{{url('assets/admin/img/logo_ori.png')}}" width="150">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-jet-nav-link href="{{ route('pengguna.index') }}">
+                        Beranda
                     </x-jet-nav-link>
                 </div>
             </div>
--- resources/views/user/layout/app.blade.php
+++ resources/views/user/layout/app.blade.php
@@ -86,7 +86,7 @@
                         <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark"></span>
                         Keranjang
                     </a>
-                    <a class="nav-icon position-relative text-decoration-none" href="#">
+                    <a class="nav-icon position-relative text-decoration-none" href="{{route('profile.show')}}">
                         <i class="fa fa-fw fa-user text-dark mr-3"></i>
                         <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark"></span>
                         Akun
