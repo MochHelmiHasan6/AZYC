7e0400c8d1c571e3b8829da65d8f2ef3a0fbf5c5
nadzul24 <nadnadzul@gmail.com>
2022-06-18 22:12:23 +0700

    add email

--- resources/views/user/layout/app.blade.php
+++ resources/views/user/layout/app.blade.php
@@ -27,16 +27,16 @@
             <div class="w-100 d-flex justify-content-between">
                 <div>
                     <i class="fa fa-envelope mx-2"></i>
-                    <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">info@company.com</a>
+                    <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:azycnomerator@gmail.com">azycnomerator@gmail.com</a>
                     <i class="fa fa-phone mx-2"></i>
                     <a class="navbar-sm-brand text-light text-decoration-none" href="tel:0851-0174-9179">0851-0174-9179</a>
                 </div>
-                <div>
+                <!-- <div>
                     <a class="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
                     <a class="text-light" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
                     <a class="text-light" href="https://twitter.com/" target="_blank"><i class="fab fa-twitter fa-sm fa-fw me-2"></i></a>
                     <a class="text-light" href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin fa-sm fa-fw"></i></a>
-                </div>
+                </div> -->
             </div>
         </div>
     </nav>
